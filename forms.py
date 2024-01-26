b442feaac5753ff320a222ee121d07af62aea521
Pedro Hernandez de la Rosa <pedrohernandexz904@gmail.com>
2024-01-25 22:08:35 -0600

    PHR 25/Enero/2024 formulario

---
+++ forms.py
@@ -0,0 +1,10 @@
+from wtforms import Form
+from wtforms import StringField, TextAreaField, SelectField, RadioField, EmailField
+
+class UserForm(Form):
+    nombre = SelectField("Nombre")
+    email = EmailField("Correo")
+    apaterno = StringField("apaterno")
+    materias = SelectField(choices=[("Espanol", "Esp",), ("Mat", "matematicas"), ('Ingles', 'ING')])
+
+    radios = RadioField('Curso', choices=[('1', '1'), ('2','2'), ('3','3')])
